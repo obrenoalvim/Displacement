13a89c1e6cb1df5431d6e35eb20c2173829e0cff
Breno Alvim <alvimbrenosilva6@gmail.com>
2023-06-16 00:22:10 -0300

    feat: table clients with pagination

--- components/TableClients/index.tsx
+++ components/TableClients/index.tsx
@@ -11,6 +11,7 @@ import {
   TableRow,
   Typography,
   Paper,
+  TablePagination,
 } from "@mui/material";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
@@ -75,6 +76,8 @@ export default function CollapsibleTable() {
   const [clientes, setClientes] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [isError, setIsError] = useState(false);
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
 
   useEffect(() => {
     getAllClients()
@@ -88,6 +91,15 @@ export default function CollapsibleTable() {
       });
   }, []);
 
+  const handleChangePage = (event: unknown, newPage: number) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
   if (isLoading) {
     return <div>Loading...</div>;
   }
@@ -95,6 +107,8 @@ export default function CollapsibleTable() {
   if (isError) {
     return <div>Error fetching data</div>;
   }
+
+  const emptyRows = rowsPerPage - Math.min(rowsPerPage, clientes.length - page * rowsPerPage);
 
   return (
     <TableContainer className={styles.tableClient} component={Paper}>
@@ -114,11 +128,29 @@ export default function CollapsibleTable() {
           </TableRow>
         </TableHead>
         <TableBody>
-          {clientes.map((cliente) => (
+          {(rowsPerPage > 0
+            ? clientes.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+            : clientes
+          ).map((cliente) => (
             <Row key={cliente["id"]} row={cliente} />
           ))}
+
+          {emptyRows > 0 && (
+            <TableRow style={{ height: 53 * emptyRows }}>
+              <TableCell colSpan={6} />
+            </TableRow>
+          )}
         </TableBody>
       </Table>
+      <TablePagination
+        rowsPerPageOptions={[5, 10, 25]}
+        component="div"
+        count={clientes.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
     </TableContainer>
   );
 }
