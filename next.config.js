2c1574a133ffb69c8e1937825d2dbd443d66fe7a
Breno Alvim <alvimbrenosilva6@gmail.com>
2023-06-19 14:34:38 -0300

    fix: change in next.config

--- next.config.js
+++ next.config.js
@@ -4,7 +4,23 @@ const path = require('path')
 
 module.exports = {
   reactStrictMode: true,
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')]
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: [
+        'style-loader',
+        'css-loader',
+        {
+          loader: 'sass-loader',
+          options: {
+            sassOptions: {
+              includePaths: [path.join(__dirname, 'styles')],
+            },
+          },
+        },
+      ],
+    });
+
+    return config;
   },
 };
