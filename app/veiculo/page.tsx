781d4779687bc70fa8315f0529f2ba40cc5ea942
Breno Alvim <alvimbrenosilva6@gmail.com>
2023-06-19 10:00:29 -0300

    fix: types error in tableClient

--- app/veiculo/page.tsx
+++ app/veiculo/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-import FormDialog from "@/components/TableClients/FormDialog";
 import ResponsiveAppBar from "../../components/Header";
 import CollapsibleTable from "../../components/TableClients";
 export default function Home() {
