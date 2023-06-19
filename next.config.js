3be8e98c5ec9834556ac59c3bdbd118f21f620bf
Breno Alvim <alvimbrenosilva6@gmail.com>
2023-06-19 14:00:55 -0300

    fix: scss/sass in export in next.config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
+const path = require('path')
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+
+const nextConfig = {
+  reactStrictMode: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  }
+}
 
 module.exports = nextConfig
