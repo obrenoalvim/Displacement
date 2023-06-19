2a33ddfbbb557b0b0384cf7a2870dc61321ccf09
Breno Alvim <alvimbrenosilva6@gmail.com>
2023-06-19 14:31:27 -0300

    fix: remove line on next.config

--- next.config.js
+++ next.config.js
@@ -5,7 +5,6 @@ const path = require('path')
 module.exports = {
   reactStrictMode: true,
   sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
-    prependData: `@import "styles/variables.scss";`, // Importa o arquivo de variáveis globais
+    includePaths: [path.join(__dirname, 'styles')]
   },
 };
