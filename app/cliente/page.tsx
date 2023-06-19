327fc0e66004d5c95900269d76a8a899f875a63f
Breno Alvim <alvimbrenosilva6@gmail.com>
2023-06-19 10:25:17 -0300

    fix: remove footer from cliente/page

--- app/cliente/page.tsx
+++ app/cliente/page.tsx
@@ -1,13 +1,11 @@
 "use client";
 import Header from "../../components/Header";
 import CollapsibleTable from "../../components/TableClients";
-import Footer from "@/components/Footer/page";
 export default function Home() {
   return (
     <>
       <Header />
       <CollapsibleTable />
-      <Footer />
     </>
   );
 }
