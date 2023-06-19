8de89acaf403c6086e9a93e5bab10faf79a1ca84
Breno Alvim <alvimbrenosilva6@gmail.com>
2023-06-19 14:38:49 -0300

    fix: add dependencies for next.config

--- next.config.js
+++ next.config.js
@@ -4,23 +4,7 @@ const path = require('path')
 
 module.exports = {
   reactStrictMode: true,
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.scss$/,
-      use: [
-        'style-loader',
-        'css-loader',
-        {
-          loader: 'sass-loader',
-          options: {
-            sassOptions: {
-              includePaths: [path.join(__dirname, 'styles')],
-            },
-          },
-        },
-      ],
-    });
-
-    return config;
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')]
   },
 };
